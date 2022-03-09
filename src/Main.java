f16277bee9b83099446efd469da9bb3a6fb2eed1
jmbury <justyna.m.bury@gmail.com>
2022-03-09 19:41:51 +0100

    add scanner file

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Jak masz na imie? ");
+        String name = scanner.nextLine();
+        System.out.println("Jak masz na nawisko? ");
+        String surname = scanner.nextLine();
+        System.out.println("Ile masz lat? ");
+        String age = scanner.nextLine();
+
+        System.out.println("Czesc" + " "+ name + " "+ surname + " masz " + age + " " + "lat");
+
+
+
+
+
+
+    }
 }
